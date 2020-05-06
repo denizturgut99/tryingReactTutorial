27a6a1be8957f94d0b1648deffdec7ff6ebcc00c
denizturgut99 <denizturgut1999@hotmail.com>
2020-05-06 15:21:42 +0200

    Add context to share a variable through out the app

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import SearchParams from './searchParams';
 import { Router, Link } from '@reach/router';
 import Details from './details';
+import ThemeContext from './themeContext';
 
 const App = () => {
+    const themeHook = useState('black');
     return (
-        <div>
-            <header>
-                <Link to="/">Trying React</Link>
-            </header>
-            <Router>
-                <SearchParams path="/"></SearchParams>
-                <Details path="/details/:id"></Details>
-            </Router>
-        </div>
+        <React.StrictMode>
+            <ThemeContext.Provider value={themeHook}>
+                <div>
+                    <header>
+                        <Link to="/">Trying React</Link>
+                    </header>
+                    <Router>
+                        <SearchParams path="/"></SearchParams>
+                        <Details path="/details/:id"></Details>
+                    </Router>
+                </div>
+            </ThemeContext.Provider>
+        </React.StrictMode>
     );
 };
 
